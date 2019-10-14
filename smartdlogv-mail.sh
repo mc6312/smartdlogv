da566506e3f3f7322398c6c110c627f079e49149
MC6312 <mc6312@gmail.com>
2019-10-14 15:33:59 +0300

    1.2.3

--- smartdlogv-mail.sh
+++ smartdlogv-mail.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 MAILTO=""
+LOGTAG="`basename $0`"
 
 if [ -z "$MAILTO" ]
 then
     EM="$0 is not configured: no mail address."
     echo "$EM"
-    #logger -t $0 "$EM"
+    logger -t "$LOGTAG" "$EM"
     exit 1
 fi
 
-smartdlogv.py -r 2>&1 |mail "$MAILTO" -s "smartdlogv"
+logger -t "$LOGTAG" "parsing smartd log(s)..."
+if ! ./smartdlogv.py -r 2>&1 |mail "$MAILTO" -s "smartdlogv"
+then
+    EC=$?
+    logger -t "$LOGTAG" "error $EC parsing smartd log(s)"
+fi
